fbc4d92bd939809711a9adba03bbdb182cef799f
koseki <kosekitau@gmail.com>
2022-05-07 23:22:22 +0900

    Hotelling multivariate

--- R/Hotelling_car.R
+++ R/Hotelling_car.R
@@ -2,6 +2,7 @@ library(car)
 data(Davis)
 Davis
 
+### 単変量の場合
 hist(Davis$weight)
 plot(density(Davis$weight))
 
@@ -14,4 +15,24 @@ a <- (Davis$weight - mu)^2 / s2 # 異常度の計算
 th <- qchisq(0.99, 1) # 自由度1のカイ二乗分布の1%水準
 plot(a, xlab="index", ylab="anomaly score") # 異常度プロット
 lines(0:200, rep(th, length(0:200)), col="red", lty=2) # 閾値の線を書く
+
+
+
+### 多変量の場合
+X <- cbind(Davis$weight, Davis$height)
+plot(X[, 1], X[, 2], pch=16, xlab="weight", ylab="height")
+
+mx <- colMeans(X) # 標本平均ベクトル
+mx
+Xc <- X - matrix(1, nrow(X), 1) %*% mx # 各データと標本平均の差
+Sx <- t(Xc) %*% Xc / nrow(X) # 標本共分散行列
+a <- rowSums((Xc %*% solve(Sx)) * Xc) # 異常度(マハラノビス距離)
+plot(a, xlab="index", ylab="anomaly score")
+lines(0:200, rep(th, length(0:200)), col="red", lty=2)
+
+a # (200, )
+
+
+
+
 
